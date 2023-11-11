1877246498eda349e1c5a03a781c4ee66a91a144
Pasindu Perakum <it21160066my.sliit.lk>
2023-11-11 17:47:42 +0530

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,34 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+//let hasDriversLicense = false;
+//const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log('I can drive');
+//if(passTest) hasDriversLicense = true;
+//if(hasDriversLicense) console.log('I can drive');
 
 //future reserved
 //const interface ='Audio';
 //const private = 56;
 //const if = 23;
+
+//functions
+
+function logger() {
+    console.log('My name is kamal');
+}
+
+logger(); //calling/invoking/running function //function call
+logger();
+
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges)
+    const juice = `Juice produced with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+//juice variable return value  replace with the fruitProcessor
+const appleJuice = fruitProcessor(5, 0); //arguments
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(3, 4);
+console.log(appleOrangeJuice);
